76089b37ad4416b06fb2f6bbb6cd1a7fb9c4de86
WadeWatts18 <tushargupta383@gmail.com>
2020-12-20 00:36:34 +0530

    Improved

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let userScore = 0; 
 let computerScore = 0;
-var userChoice;
-var userName = prompt("Enter user name: ");
+// var userChoice;
+var user_span;
+var userName = prompt("Enter Player Name: ");
 const userLabel_id = document.getElementById("user-label");
 	userLabel_id.innerHTML = userName;
 const userScore_span = document.getElementById("user-score");
@@ -21,7 +22,29 @@ function getComputerChoice() {
 	const randomNumber = Math.floor(Math.random() * 3);
 	return choices[randomNumber];
 }
-
+// var but = document.querySelector("reset");
+// function reset () {
+// 	but.addEventListener('click',function () {
+// 		userScore = 0;
+// 		computerScore = 0;
+// 	})
+// }
+// reset();
+function reset () {
+	if(userScore == 5 || computerScore == 5){
+		
+		if(userScore == 5){
+			result_p.innerHTML = `USER WON!! <br>**GAME RESET**`;	
+		}
+		if(computerScore == 5){
+			result_p.innerHTML = `Computer WON!! <br>**GAME RESET**`;	
+		}		
+		userScore = 0;
+		computerScore = 0;
+		userScore_span.innerHTML = userScore;
+		computerScore_span.innerHTML = computerScore;
+	}
+};
 function win (userChoice, computerChoice) {
 	userScore++;
 	userScore_span.innerHTML = userScore;
@@ -29,29 +52,33 @@ function win (userChoice, computerChoice) {
 
 	// `` is used to display string without using + or "" but to 
 	// display variable it should be inside ${}
-	result_p.innerHTML = `${userName} picked ${userChoice}. Computer picked ${computerChoice}.   <br><br> ${userName} wins!!`;
+	
+
+	result_p.innerHTML = `${userName} picked ${userChoice} : Computer picked ${computerChoice}.   <br><br> ${userName} wins!!`;
 	document.getElementById(userChoice).classList.add("green-glow");
 	setTimeout(function(){ document.getElementById(userChoice).classList.remove("green-glow")}, 300);
+	reset();
 }
 
 function lose (userChoice, computerChoice) {
 	computerScore++;
 	computerScore_span.innerHTML = userScore;
 	computerScore_span.innerHTML = computerScore;
-	result_p.innerHTML = `${userName} picked ${userChoice}. Computer picked ${computerChoice}.   <br><br> ${userName} loses!!`;
+	result_p.innerHTML = `${userName} picked ${userChoice} : Computer picked ${computerChoice}.   <br><br> ${userName} loses!!`;
 	document.getElementById(userChoice).classList.add("red-glow");
 	setTimeout(function(){ document.getElementById(userChoice).classList.remove("red-glow")}, 300);
+	reset();
 }
 
-function draw (userChoice, computerChoice) {
-	userScore = 0;
-	computerScore = 0; 
+function draw (userChoice, computerChoice) { 
 	userScore_span.innerHTML = userScore;
 	computerScore_span.innerHTML = computerScore;
-	result_p.innerHTML = `${userName} picked ${userChoice}. Computer picked ${computerChoice}.   <br><br> DRAW!! ** GAME RESET **`;
+	result_p.innerHTML = `${userName} picked ${userChoice} : Computer picked ${computerChoice}.   <br><br> DRAW!!`;
 	document.getElementById(userChoice).classList.add("gray-glow");
 	setTimeout(function(){ document.getElementById(userChoice).classList.remove("gray-glow")}, 300);
+	reset();
 }
+
 
 
 function game (userChoice) {
@@ -63,7 +90,7 @@ function game (userChoice) {
 			win(userChoice, computerChoice);
 			break;
 		case "RockPaper"://rp
-		case "PaperScissore"://ps
+		case "PaperScissors"://ps
 		case "ScissorsRock"://sr
 			lose(userChoice, computerChoice);
 			break;
